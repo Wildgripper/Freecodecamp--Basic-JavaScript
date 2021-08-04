cfd369a091f2d79a12f2502d7c4b3f7dfba52c34
Petar Manoilov <manoilov@gmail.com>
2021-08-04 23:18:10 +0300

    myMusic

---
+++ myMusic.js
@@ -0,0 +1,24 @@
+var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Nina Nikolina",
+      "title": "Antology",
+      "release_year": 2011,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
