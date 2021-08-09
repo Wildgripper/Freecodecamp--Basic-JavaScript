b52512ab2df00e4179fecdad9567383b6d601559
Petar Manoilov <manoilov@gmail.com>
2021-08-09 23:17:53 +0300

    multiplyAll

--- loop.js
+++ loop.js
@@ -1,7 +1,14 @@
-let myArr = [ 2, 3, 4, 5, 6];
+function multiplyAll(arr) {
+  let product = 1;
+  
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      product *= arr[i][j];
+    }
+  }
 
-let total = 0;
-
-for (let i = 0; i < myArr.length; i++) {
-  total += myArr[i];
+  console.log(product);
+  return product;
 }
+
+multiplyAll([[1,2],[3,4],[5,6,7]]);
