5333b79d83c63b69816f714f49477812bb89ab74
Petar Manoilov <manoilov@gmail.com>
2021-08-19 22:56:02 +0300

    checkSign

--- checkEqual.js
+++ checkEqual.js
@@ -1,5 +1,7 @@
-function checkEqual(a, b) {
-    return a === b ? "Equal" : "Not Equal";
+function checkSign(num) {
+    return (num === 0) ? "zero"
+    : (num > 0) ? "positive"
+    : "negative";
   }
   
-  checkEqual(1, 2);
+  checkSign(10);
