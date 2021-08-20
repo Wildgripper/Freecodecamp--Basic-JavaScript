be89e95acbce52f6f6a6dfc6b757b269b322fe4e
Petar Manoilov <manoilov@gmail.com>
2021-08-20 23:47:33 +0300

    countdown

---
+++ countdown.js
@@ -0,0 +1,12 @@
+// Only change code below this line
+function countdown(n){
+    let array = [];
+    if (n < 1) {
+        return array;
+    }
+
+
+    
+    return;
+  }
+  // Only change code above this line
