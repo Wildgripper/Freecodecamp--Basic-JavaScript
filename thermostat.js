ea86557e183253abeca7b6687ddd7cf21af07bfb
Petar Manoilov <manoilov@gmail.com>
2021-09-12 22:28:23 +0300

    Thermostat

---
+++ thermostat.js
@@ -0,0 +1,19 @@
+class Thermostat {
+    constructor(fahrenheit) {
+      this._temperature = 5/9 * (fahrenheit - 32);
+    }
+  
+    get temperature() {
+      return this._temperature;
+    }
+  
+    set temperature(temperature) {
+      this._temperature = temperature;
+    }
+  
+  }
+  
+  const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+  let temp = thermos.temperature; // 24.44 in Celsius
+  thermos.temperature = 26;
+  temp = thermos.temperature; // 26 in Celsius
