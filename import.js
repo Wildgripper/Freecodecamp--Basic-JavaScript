b2fb89eb4aac8414ef11fa7eab0afbd90155b38a
Petar Manoilov <manoilov@gmail.com>
2021-09-16 23:46:34 +0300

    Import default

--- import.js
+++ import.js
@@ -1,4 +1,5 @@
-import * as stringFunctions from './string_functions.js'
+import * as stringFunctions from './string_functions.js';
+import subtract from './string_functions.js';
 
 stringFunctions.uppercaseString("hello");
 stringFunctions.lowercaseString("WORLD!");
