99bd58a4fbe06cb9c71991ca0025a4101b13678d
Petar Manoilov <manoilov@gmail.com>
2021-09-19 23:32:54 +0300

    waldoRegex

--- myRegex.js
+++ myRegex.js
@@ -1,3 +1,3 @@
-let myString = "Hello, World!";
-let myRegex = /Hello/;
-let result = myRegex.test(myString); 
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; 
+let result = waldoRegex.test(waldoIsHiding);
