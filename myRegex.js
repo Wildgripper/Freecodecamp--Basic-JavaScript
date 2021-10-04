8a162a266f5fed879b1531debfd8209405f3766a
Petar Manoilov <manoilov@gmail.com>
2021-10-04 23:10:42 +0300

    caboose

--- myRegex.js
+++ myRegex.js
@@ -1,3 +1,3 @@
-let rickyAndCal = "Cal and Ricky both like racing.";
-let calRegex = /^Cal/; 
-let result = calRegex.test(rickyAndCal);
+let caboose = "The last car on a train is the caboose";
+let lastRegex = /caboose$/;
+let result = lastRegex.test(caboose);
