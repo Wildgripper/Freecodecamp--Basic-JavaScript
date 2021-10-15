705be73167f48167319f5cecce146b8b0dcb2239
Petar Manoilov <manoilov@gmail.com>
2021-10-15 23:44:28 +0300

    /[a-z0-9]/ig

--- myRegex.js
+++ myRegex.js
@@ -1,11 +1,13 @@
 /*
 Usernames can only use alpha-numeric characters.
-The only numbers in the username have to be at the end. There can be zero or more of them at the end. Username cannot start with the number.
+The only numbers in the username have to be at the end. 
+There can be zero or more of them at the end. Username cannot start with the number.
 Username letters can be lowercase and uppercase.
 Usernames have to be at least two characters long. A two-character username can only use alphabet letters as characters.
 */
 
 let username = "JackOfAllTrades";
-let userCheck = /JackOfAllTrades/; 
+let userCheck = /[a-z0-9]/ig;  
 let result = userCheck.test(username);
 console.log(result);
+console.log(username.match(userCheck));
