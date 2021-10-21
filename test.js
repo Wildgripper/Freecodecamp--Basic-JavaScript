f23a7dc272b88bb006761f381a21e1cee201c042
Petar Manoilov <manoilov@gmail.com>
2021-10-21 23:02:10 +0300

    promise

--- test.js
+++ test.js
@@ -1,13 +1,10 @@
-let obj = {
-    a: "foo",
-    b: 12,
-    c: "bar"
-};
-
-let {
-    c,
-    a,
-    b
-} = obj;
-
-console.log(c);
+const p1 = new Promise((resolve) => {
+    setTimeout(resolve, 3000, 'one');
+});
+const p2 = new Promise((resolve) => {
+    setTimeout(resolve, 2000, 'two');
+});
+const p3 = new Promise((res, reject) => {
+    setTimeout(() => reject('rejected'), 2000, 'three');
+});
+Promise.race([p1, p2, p3]).then(v => console.log('Success:', v)).catch(err => console.log('Error:', err)).then(() => console.log('Demo finished '));
