258c700aa543f18d2163b656d9b8784529962809
Petar Manoilov <manoilov@gmail.com>
2021-10-29 22:54:48 +0300

    mixedNumbers

--- yourArray.js
+++ yourArray.js
@@ -1,3 +1,7 @@
-let myArray = ["a", "b", "c", "d"];
-myArray[1] = "other b"
-console.log(myArray);
+function mixedNumbers(arr) {
+      arr.unshift('I', 2, 'three');
+      arr.push(7, 'VIII', 9);
+    return arr;
+  }
+  
+  console.log(mixedNumbers(['IV', 5, 'six']));
