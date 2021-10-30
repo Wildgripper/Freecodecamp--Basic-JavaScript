5571cf5d9811c89ae8e77ee324be3eb56a39e1d3
Petar Manoilov <manoilov@gmail.com>
2021-10-30 23:40:23 +0300

    checkInventory

--- objects.js
+++ objects.js
@@ -1,17 +1,14 @@
-// Here we've defined an object userActivity, which includes another object nested within it. 
-// Set the value of the online key to 45.
-
-let userActivity = {
-    id: 23894201352,
-    date: 'January 1, 2017',
-    data: {
-        totalUsers: 51,
-        online: 42
-    }
-};
-
-// Only change code below this line
-    userActivity.data.online = 45;
-// Only change code above this line
-
-console.log(userActivity);
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+  };
+  
+  function checkInventory(scannedItem) {
+    return foods[scannedItem];
+  }
+  
+  console.log(checkInventory("apples"));
