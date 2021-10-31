a734fd161af54f8cbc53fbaca34d95d64fa430ac
Petar Manoilov <manoilov@gmail.com>
2021-10-31 20:21:28 +0200

    getArrayOfUsers

--- objects.js
+++ objects.js
@@ -1,11 +1,24 @@
-function countOnline(usersObj) {
-  let number = 0;
-  for (let user in usersObj) {
-
-    if (usersObj[user].online === true) {
-      number++;
-    }
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
   }
-  return number;
+};
+
+function getArrayOfUsers(obj) {
+  return Object.keys(obj);
 }
 
+console.log(getArrayOfUsers(users));
