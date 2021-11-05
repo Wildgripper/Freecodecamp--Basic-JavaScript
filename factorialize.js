2b95e581714bcbec55bb659f1b38b6354fee9c02
Petar Manoilov <manoilov@gmail.com>
2021-11-05 23:48:54 +0200

    result

--- factorialize.js
+++ factorialize.js
@@ -1,7 +1,11 @@
-// Return the factorial of the provided integer.
-
 function factorialize(num) {
-    return num;
+    let result = 1;
+    for(let i = 1; i <= num; i++) {
+        result = result * i;
+    }
+
+    // console.log(result);
+    return result;
   }
   
   factorialize(5);
