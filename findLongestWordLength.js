a3ea35058d39177646cd1d0dfbe447988274bd33
Petar Manoilov <manoilov@gmail.com>
2021-11-08 23:39:03 +0200

    regex

--- findLongestWordLength.js
+++ findLongestWordLength.js
@@ -1,4 +1,11 @@
+// Return the length of the longest word in the provided sentence.
+// Your response should be a number.
+
 function findLongestWordLength(str) {
+  let regEx = /(\w+)/;
+  console.log(str.match(regEx));
+
+
     return str.length;
   }
   
