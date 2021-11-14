1de5e3d36e9cf24658c2e39acf60ab56617510ce
Petar Manoilov <manoilov@gmail.com>
2021-11-14 13:23:12 +0200

    Solution

--- largestOfFour.js
+++ largestOfFour.js
@@ -1,28 +1,16 @@
-// Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
-// Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
-
 function largestOfFour(arr) {
     let newArray = [];
-    for (i = 0; i < arr.length; i++) {
-        console.log(arr[i]);
-        newArray.push(arr[i][0]);
-        let maxNum = 0;
+    let maxNum;
+    for (let i = 0; i < arr.length; i++) {
+        maxNum = arr[i][0];
         for (let j = 0; j < arr[i].length; j++) {
             if (arr[i][j] > maxNum) {
                 maxNum = arr[i][j];
             }
-            newArray.push(maxNum);
-
         }
+        newArray.push(maxNum);
     }
-
-    console.log(newArray);
-    return arr;
+    return newArray;
 }
 
-largestOfFour([
-    [4, 5, 1, 3],
-    [13, 27, 18, 26],
-    [32, 35, 37, 39],
-    [1000, 1001, 857, 1]
-]);
+largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]);
